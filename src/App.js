c9938cff63b4004a7f9621a6be3d2e1dfdc793e7
afeef <afeef@sanoft.com>
2021-10-26 23:08:25 +0530

    feat: app created and js actions done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [expenceList, setExpenceList] = useState([]);
+  const [expence, setExpence] = useState({});
+  const handleExpenceChange = (event) =>
+    setExpence({
+      ...expence,
+      [event.target.name]: event.target.value,
+    });
+  const clearExpence = () => setExpence({});
+  const handleAddExpence = () => {
+    setExpenceList(expenceList.length ? [...expenceList, expence] : [expence]);
+    clearExpence();
+  };
+  const getSumTotal = (list) => {
+    const values = list.map((expence) => expence.amount);
+    return values.reduce(
+      (previousValue, currentValue) =>
+        Number(previousValue) + Number(currentValue),
+      0
+    );
+  };
+
+  const getTotalIncome = () =>
+    getSumTotal(expenceList.filter((expence) => expence.amount >= 0));
+  const getTotalExpence = () =>
+    getSumTotal(expenceList.filter((expence) => expence.amount < 0));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <p>{expenceList.length ? getTotalIncome() : null}</p>
+        <p>{expenceList.length ? getTotalExpence() : null}</p>
+      </div>
+      <div>
+        <h1>Add expence</h1>
+        <div>
+          <input
+            type="text"
+            name="label"
+            value={expence?.label || ""}
+            onChange={handleExpenceChange}
+          />
+          <input
+            type="number"
+            name="amount"
+            value={expence?.amount || ""}
+            onChange={handleExpenceChange}
+          />
+        </div>
+        <button onClick={handleAddExpence}>Add</button>
+      </div>
+      <div>
+        {expenceList.length
+          ? expenceList.map((expence) => (
+              <div>
+                <h3>{expence.label}</h3>
+                <span>{expence.amount}</span>
+              </div>
+            ))
+          : null}
+      </div>
     </div>
   );
 }
